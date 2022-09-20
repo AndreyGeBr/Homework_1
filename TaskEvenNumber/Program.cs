fa2e17c5217f63b6aaf93eb215a6c1ffde9f04cb
Andrey <a89615076005@yandex.ru>
2022-09-20 12:52:34 +0300

    Add task Even Number

---
+++ TaskEvenNumber/Program.cs
@@ -0,0 +1,17 @@
+﻿// Программа которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+Console.Clear();
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+int rem = number % 2;
+
+if (rem == 0)
+{
+    Console.Write("Да");
+}
+else
+{
+    Console.Write("Нет");
+}
+
